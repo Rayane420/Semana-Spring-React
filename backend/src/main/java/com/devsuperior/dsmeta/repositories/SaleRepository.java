b39e70a5ae8cc22243839fd405ec2519cc87c055
Rayane <rayanemaciel10@gmail.com>
2022-07-17 14:54:23 -0300

    Add sms com Twilio

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-    //Buscando 20 maiores vendas
     @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
     Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
