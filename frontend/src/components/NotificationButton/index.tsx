9ccea95c45388891e59bb94c5a47c19f58e61a5e
Rayane <rayanemaciel10@gmail.com>
2022-07-18 20:35:42 -0300

    add sent SMS notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("200SUCESS");
+            toast.info("SMS enviado com Sucesso!");
         });
 }
 function NotificationButton({ saleId }: Props) {
